934d6576011bb916d2d254be5a73c298d4954929
coolpagani <coolpagani@gmail.com>
2022-09-03 13:25:46 +0300

    Закончили 25 задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿double Pow1(int a, int b)
+{
+    double result = 0;
+    result = Math.Pow(a, b);
+    return result;
+}
+int.TryParse(Console.ReadLine(), out int A);
+int.TryParse(Console.ReadLine(), out int B);
+Console.Write(Pow1(A, B));
